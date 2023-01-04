36ba134a11e3c17f2f7d0594dc76b33e38109298
Piotr Dryja <dragon-83@o2.pl>
2023-01-04 20:29:47 +0100

    PD-1 init commit, added spring boot framework

---
+++ src/main/java/pl/dryja/patternsearcher/PatternsearcherApplication.java
@@ -0,0 +1,13 @@
+package pl.dryja.patternsearcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatternsearcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatternsearcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/dryja/patternsearcher/PatternsearcherApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.dryja.patternsearcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatternsearcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
